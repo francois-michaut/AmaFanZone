0c303c71065ea14ce3b0344977c65b78ba225a0a
francois-michaut <f.michaut@orange.fr>
2022-03-18 12:01:43 +0100

    Page du formulaire

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// const app = {
+//     init: function()
+//     {
+//         const signInButton = document.querySelector( '.connect-button' );
+
+//         signInButton.addEventListener( 'click' , app.handleConnectionButton );
+//     },
+
+//     handleConnectionButton: function()
+//     {
+//         alert( 'nouvelle pop-up!' );
+//     }
+// }
+
+
+// document.addEventListener( "DOMContentLoaded", app.init );
+
